b0512036d182002c0e655d9ffaec90bc7e3cb5ac
RiosGabri <grp@cesar.school>
2026-05-05 14:48:51 -0300

    fix: fixing linter

--- backend/project/wsgi.py
+++ backend/project/wsgi.py
@@ -14,7 +14,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(BASE_DIR))
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application  # noqa: E402
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
